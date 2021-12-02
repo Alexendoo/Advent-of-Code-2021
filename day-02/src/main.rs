3a1d5f5273876ab621f1d2833e85e7630fd2b1bf
Alex Macleod <alex@macleod.io>
2021-12-02 11:47:19 +0000

    Day 02 Part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,18 +1,35 @@
 fn main() {
     let input = include_str!("input");
 
-    let (x, depth) = input
+    let commands: Vec<(&str, usize)> = input
         .lines()
         .map(|line| {
             let (direction, by) = line.split_once(" ").unwrap();
             (direction, by.parse::<usize>().unwrap())
         })
-        .fold((0, 0), |(x, depth), (direction, by)| match direction {
+        .collect();
+
+    let (x, depth) = commands
+        .iter()
+        .fold((0, 0), |(x, depth), &(direction, by)| match direction {
             "forward" => (x + by, depth),
             "down" => (x, depth + by),
             "up" => (x, depth - by),
-            _ => unreachable!()
+            _ => unreachable!(),
         });
 
     println!("Part 1: {}", x * depth);
+
+    let (x, depth, _) =
+        commands.iter().fold(
+            (0, 0, 0),
+            |(x, depth, aim), &(direction, by)| match direction {
+                "forward" => (x + by, depth + aim * by, aim),
+                "down" => (x, depth, aim + by),
+                "up" => (x, depth, aim - by),
+                _ => unreachable!(),
+            },
+        );
+
+    println!("Part 2: {}", x * depth);
 }
