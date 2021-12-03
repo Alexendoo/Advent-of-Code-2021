8633f49a2d85d51835f3cd379f3c247dbae0347b
Alex Macleod <alex@macleod.io>
2021-12-03 12:15:33 +0000

    Day 03 Part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,27 @@
+use std::cmp::Ordering;
+
+fn ones(numbers: &[usize], pos: usize) -> Ordering {
+    let count = numbers
+        .iter()
+        .filter(|&number| (number >> pos) & 1 == 1)
+        .count();
+
+    (count * 2).cmp(&numbers.len())
+}
+
+fn rating(mut numbers: Vec<usize>, width: usize, compare: fn(Ordering) -> bool) -> usize {
+    for pos in (0..width).rev() {
+        let keep = compare(ones(&numbers, pos)) as usize;
+        numbers.retain(|&number| (number >> pos) & 1 == keep);
+
+        if numbers.len() == 1 {
+            break;
+        }
+    }
+
+    numbers[0]
+}
+
 fn main() {
     let input = include_str!("input");
 
@@ -10,16 +34,16 @@ fn main() {
 
     let mut gamma = 0;
     for pos in 0..width {
-        let ones = numbers
-            .iter()
-            .filter(|&number| number & (1 << pos) > 0)
-            .count();
-
-        if ones > numbers.len() / 2 {
-            gamma |= 1 << pos
+        if ones(&numbers, pos).is_gt() {
+            gamma |= 1 << pos;
         }
     }
     let epsilon = !gamma & !(!0 << width);
 
-    println!("Part 1: {}", gamma * epsilon)
+    println!("Part 1: {}", gamma * epsilon);
+
+    let oxygen = rating(numbers.clone(), width, Ordering::is_ge);
+    let co2 = rating(numbers, width, Ordering::is_lt);
+
+    println!("Part 2: {}", oxygen * co2);
 }
