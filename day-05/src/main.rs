d9641fb8f6b389d413196809e2271f3be3cb2420
Alex Macleod <alex@macleod.io>
2021-12-05 12:04:36 +0000

    Day 05 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,11 +1,35 @@
+use std::cmp::max;
 use std::collections::HashMap;
+
+type Vent = (i32, i32);
+type Vents = HashMap<Vent, i32>;
+
+fn plot(vents: &mut Vents, (x1, y1): Vent, (x2, y2): Vent) {
+    let dx = x2.cmp(&x1) as i32;
+    let dy = y2.cmp(&y1) as i32;
+
+    let count = max((x1 - x2).abs(), (y1 - y2).abs());
+    for i in 0..=count {
+        let x = x1 + i * dx;
+        let y = y1 + i * dy;
+        *vents.entry((x, y)).or_default() += 1;
+    }
+}
+
+fn overlaps(vents: &Vents) -> usize {
+    vents.values().filter(|&&c| c > 1).count()
+}
+
+fn is_diagonal((x1, y1): Vent, (x2, y2): Vent) -> bool {
+    x1 != x2 && y1 != y2
+}
 
 fn main() {
     let input = include_str!("input");
 
     let mut numbers = input
         .split(|ch: char| !ch.is_ascii_digit())
-        .flat_map(|digit| digit.parse::<usize>())
+        .flat_map(|digit| digit.parse::<i32>())
         .peekable();
 
     let mut lines = Vec::new();
@@ -15,24 +39,20 @@ fn main() {
         lines.push(((next(), next()), (next(), next())));
     }
 
-    let mut vents: HashMap<(usize, usize), usize> = HashMap::new();
-    for &((x1, y1), (x2, y2)) in &lines {
-        println!("{},{} -> {},{}", x1, y1, x2, y2);
-
-        if x1 == x2 {
-            for y in y1.min(y2)..=y1.max(y2) {
-                *vents.entry((x1, y)).or_default() += 1;
-            }
-        }
-
-        if y1 == y2 {
-            for x in x1.min(x2)..=x1.max(x2) {
-                *vents.entry((x, y1)).or_default() += 1;
-            }
+    let mut vents: Vents = HashMap::new();
+    for &(a, b) in &lines {
+        if !is_diagonal(a, b) {
+            plot(&mut vents, a, b);
         }
     }
 
-    let overlaps = vents.values().filter(|&&c| c > 1).count();
+    println!("Part 1: {}", overlaps(&vents));
 
-    println!("Part 1: {}", overlaps);
+    for &(a, b) in &lines {
+        if is_diagonal(a, b) {
+            plot(&mut vents, a, b);
+        }
+    }
+
+    println!("Part 2: {}", overlaps(&vents));
 }
