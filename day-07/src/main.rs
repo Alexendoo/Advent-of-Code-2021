82d2b84f1f406fba06d330e8803852367f812ca7
Alex Macleod <alex@macleod.io>
2021-12-07 11:18:27 +0000

    Day 07 Part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -10,5 +10,17 @@ fn main() {
     crabs.sort();
     let mode = crabs[crabs.len() / 2];
 
-    println!("Part 1: {}", crabs.iter().map(|x| (x - mode).abs()).sum::<i32>());
+    println!(
+        "Part 1: {}",
+        crabs.iter().map(|x| (x - mode).abs()).sum::<i32>()
+    );
+
+    let mean = crabs.iter().sum::<i32>() / crabs.len() as i32;
+
+    let consumption = |x: &i32| {
+        let diff = (x - mean).abs();
+        (diff * (diff + 1)) / 2
+    };
+
+    println!("Part 2: {}", crabs.iter().map(consumption).sum::<i32>());
 }
