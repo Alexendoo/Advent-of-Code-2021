b40ea820e5b45678e79072581d70dfc736bde3c3
Alex Macleod <alex@macleod.io>
2021-12-10 11:43:52 +0000

    Day 10 Part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,4 +1,10 @@
-fn parse(line: &str) -> Option<char> {
+enum Parsed {
+    Error(char),
+    Incomplete(Vec<char>),
+}
+use Parsed::{Error, Incomplete};
+
+fn parse(line: &str) -> Parsed {
     let mut stack = Vec::new();
     for ch in line.chars() {
         match ch {
@@ -8,29 +14,53 @@ fn parse(line: &str) -> Option<char> {
             '<' => stack.push('>'),
             ch => {
                 if stack.pop() != Some(ch) {
-                    return Some(ch);
+                    return Error(ch);
                 }
             }
         }
     }
 
-    None
+    Incomplete(stack)
 }
 
 fn main() {
     let input = include_str!("input");
 
-    let score: usize = input
-        .lines()
-        .filter_map(parse)
-        .map(|ch| match ch {
-            ')' => 3,
-            ']' => 57,
-            '}' => 1197,
-            '>' => 25137,
-            _ => unreachable!(),
+    let results: Vec<Parsed> = input.lines().map(parse).collect();
+
+    let error_score: usize = results
+        .iter()
+        .map(|parsed| match parsed {
+            Error(')') => 3,
+            Error(']') => 57,
+            Error('}') => 1197,
+            Error('>') => 25137,
+            _ => 0,
         })
         .sum();
 
-    println!("Part 1: {}", score);
+    println!("Part 1: {}", error_score);
+
+    let mut scores: Vec<usize> = results
+        .into_iter()
+        .filter_map(|parsed| match parsed {
+            Incomplete(stack) => Some(stack),
+            Error(_) => None,
+        })
+        .map(|stack| {
+            stack.into_iter().rev().fold(0, |acc, ch| {
+                acc * 5
+                    + match ch {
+                        ')' => 1,
+                        ']' => 2,
+                        '}' => 3,
+                        '>' => 4,
+                        _ => unreachable!(),
+                    }
+            })
+        })
+        .collect();
+    scores.sort_unstable();
+
+    println!("Part 2: {}", scores[scores.len() / 2]);
 }
